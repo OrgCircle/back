e54311fc65a3b39c1f3908ec2ed87bcee6e47ebf
FabienTreve <trevefabien@gmail.com>
2020-11-27 12:39:50 +0100

    Feature List

---
+++ src/entity/ListType.ts
@@ -0,0 +1,39 @@
+import { Document, Schema, model } from "mongoose";
+import { Field, ObjectType } from "../../lib";
+import { Input } from "../../lib/decorators/Input";
+
+export interface IListType extends Document {
+  label: string;
+  icon: string;
+}
+
+export const ListTypeSchema: Schema = new Schema<IListType>(
+  {
+    label: { type: String, required: true, unique: true },
+    icon: { type: Array, required: true },
+  },
+  { timestamps: false }
+);
+
+@Input
+export class ListTypeInput implements Partial<IListType> {
+  @Field({ description: "Label of the list type" })
+  label: string;
+
+  @Field({ description: "Icon of the list type" })
+  icon: string;
+}
+
+@ObjectType
+export class ListTypeObject implements Partial<IListType> {
+  @Field({ description: "Object identifier" })
+  _id: string;
+
+  @Field({ description: "Label of the list type" })
+  label: string;
+
+  @Field({ description: "Icon of the list type" })
+  icon: string;
+}
+
+export default model<IListType>("ListType", ListTypeSchema);
