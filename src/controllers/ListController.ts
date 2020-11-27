dddfd1d1ab34d6242a33af5799fe2cec08010993
Martin PELCAT <martinpelcat@hotmail.com>
2020-11-27 17:54:25 +0100

    change response

--- src/controllers/ListController.ts
+++ src/controllers/ListController.ts
@@ -11,26 +11,32 @@ import {
   Post,
   Patch,
 } from "../../lib";
-import { IList, ListInput } from "../entity/List";
+import { IList, ListInput, ListObject } from "../entity/List";
 import { JWTPayload } from "../helpers/jwt";
 import { ListService } from "../services/ListService";
 
 @Controller("/list")
 export class ListController {
-  constructor(private ListService: ListService) {}
+  constructor(private listService: ListService) {}
 
   @Get("/", { description: "Return all lists in database" })
   @Authorized()
-  async getLists(@Ctx { res }: ContextType) {
+  async getLists(@Ctx { res }: ContextType): HttpResponse<ListObject[]> {
     const { famillyId }: JWTPayload = res.locals.user;
-    return await this.ListService.getAllLists(famillyId);
+    const lists = await this.listService.getAllLists(famillyId);
+    return { code: 200, data: lists };
   }
 
   @Get("/:id", { description: "Return the list matching the id" })
   @Authorized()
-  async getList(@Param("id") id: string, @Ctx { res }: ContextType) {
+  async getList(
+    @Param("id") id: string,
+    @Ctx { res }: ContextType
+  ): HttpResponse<ListObject> {
     const { famillyId }: JWTPayload = res.locals.user;
-    return await this.ListService.getListById(id, famillyId);
+    const list = await this.listService.getListById(id, famillyId);
+    if (!list) return { code: 404, error: "List not found" };
+    return { code: 200, data: list };
   }
 
   @Post("/", { description: "Create a list" })
@@ -38,10 +44,10 @@ export class ListController {
   async postList(
     @Body { name, content, listType }: ListInput,
     @Ctx { res }: ContextType
-  ) {
+  ): HttpResponse<ListObject> {
     console.log(content, listType);
     const { famillyId }: JWTPayload = res.locals.user;
-    const insertedList = await this.ListService.createList(
+    const insertedList = await this.listService.createList(
       {
         name,
         content,
@@ -50,7 +56,7 @@ export class ListController {
       famillyId
     );
 
-    return insertedList;
+    return { code: 201, data: insertedList };
   }
 
   @Patch("/:id", { description: "Edit the list matching the id" })
@@ -59,19 +65,19 @@ export class ListController {
     @Param("id") id: string,
     @Body { name, content, listType }: ListInput,
     @Ctx { res }: ContextType
-  ) {
+  ): HttpResponse<ListObject> {
     const { famillyId }: JWTPayload = res.locals.user;
     const list: Partial<IList> = {};
     if (name) list.name = name;
     if (content) list.content = content;
     if (listType) list.listType = listType;
-    const updatedList = await this.ListService.updateListById(
+    const updatedList = await this.listService.updateListById(
       id,
       list,
       famillyId
     );
 
-    return updatedList;
+    return { code: 201, data: updatedList };
   }
 
   @Delete("/:id", { description: "Delete the list matching the id" })
@@ -81,7 +87,7 @@ export class ListController {
     @Ctx { res }: ContextType
   ): HttpResponse<null> {
     const { famillyId }: JWTPayload = res.locals.user;
-    await this.ListService.deleteListById(id, famillyId);
+    await this.listService.deleteListById(id, famillyId);
     return { code: 204, data: null };
   }
 }
