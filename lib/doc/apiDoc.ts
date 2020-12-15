c8d7b984d9c23322ac3c6b39993c9432912fe0f1
Martin PELCAT <martinpelcat@hotmail.com>
2020-12-15 15:16:01 +0100

    oui

--- lib/doc/apiDoc.ts
+++ lib/doc/apiDoc.ts
@@ -25,7 +25,7 @@ export const generateApiDoc = (router: Router, docUrl: string) => {
       (controller) => controller.target.name === req.params.name
     );
     if (controller) {
-      res.render(__dirname + "./views/controller.pug", {
+      res.render(__dirname + "/views/controller.pug", {
         controller,
         docUrl,
       });
